edfa00cb11636043b4031133f0b4d9e9e66c708e
Arthur <arthurleonel7@gmail.com>
2023-08-05 15:47:43 -0300

    projeto configurado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Cidade" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
